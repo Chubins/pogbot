5ae8581321fabc6409d4303c5730e57a0ea204f2
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-11-13 21:18:02 +0000

    try to add index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+require("dotenv").config();
+
+const client = new Discord.Client();
+
+const prefix = '.';
+
+client.once('ready', () => {
+    console.log('pogbot is online!');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split("/ +/");
+    const command = args.shift().toLowerCase();
+
+    if(command === 'pog'){
+        message.channel.send('https://i.imgur.com/Pp3Ra0N.png');
+    } else if (command == 'pogbotinfo'){
+        message.channel.send('pogbot was made by Chubins#8664. His website is https://www.chubins.tk. His discord is https://discord.gg/uUK9UVV . Add pogbot to your server here: https://www.chubins.tk/projects')
+    }
+});
+
+client.login(process.env.bottoken)
+
+const keepAlive = require('./server');
+keepAlive();
